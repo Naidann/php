e6d555f727b1ba90cf87ebbda7a5d79b6047f122
Naidann <98400114+Naidann@users.noreply.github.com>
2022-04-24 21:15:07 +0300

    summ

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+     header('Content-Type: text/html; charset=utf-8');
+     $files file_get_contents('count.txt');
+     for ($i=0; $i<count($files); $i++) {
+     $files[$i];
+        
+     }
+?>
